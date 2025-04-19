1009e9d353c495fbf4f4be405b92a8232fc12446
N1ceGel <rabazhenov5@gmail.com>
2025-04-19 19:00:55 +0500

    FAQpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,132 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Competitions from './components/Competitions';
-import Courses from './components/Courses';
-import Footer from './components/Footer';
-import ForWho from './components/ForWho';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import News from './components/News';
-import CompetitionHero from './components/CompetitionHero';
-import CompetitionTabs from './components/competitionTabs';
-import СompetitionTabsNow from './components/СompetitionTabsNow.jsx';
-import CompetitionList from './components/CompetitionList.jsx';
-import LearningHero from './components/LearningHero.jsx';
-import BeginnerCourses from './components/BeginnerCourses.jsx';
-import BeginnerCoursesTabsNow from './components/BeginnerCoursesTabsNow.jsx';
-import BeginnerCourseslist from './components/BeginnerCourseslist.jsx';
-import ComHero from './components/ComHero.jsx';
-import TabMenu from './components/TabMenu.jsx';
-import Course from './components/Course.jsx';
-import './index.css';
-import TabMenuCourse from './components/TabMenuCourse.jsx';
-import Registration from './components/Registration.jsx';
-import Login from './components/Login.jsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Competitions from "./components/Competitions";
+import Courses from "./components/Courses";
+import Footer from "./components/Footer";
+import ForWho from "./components/ForWho";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import News from "./components/News";
+import CompetitionHero from "./components/CompetitionHero";
+import CompetitionTabs from "./components/competitionTabs";
+import СompetitionTabsNow from "./components/СompetitionTabsNow.jsx";
+import CompetitionList from "./components/CompetitionList.jsx";
+import LearningHero from "./components/LearningHero.jsx";
+import BeginnerCourses from "./components/BeginnerCourses.jsx";
+import BeginnerCoursesTabsNow from "./components/BeginnerCoursesTabsNow.jsx";
+import BeginnerCourseslist from "./components/BeginnerCourseslist.jsx";
+import ComHero from "./components/ComHero.jsx";
+import TabMenu from "./components/TabMenu.jsx";
+import Course from "./components/Course.jsx";
+import "./index.css";
+import TabMenuCourse from "./components/TabMenuCourse.jsx";
+import Registration from "./components/Registration.jsx";
+import Login from "./components/Login.jsx";
+import FAQpage from "./pages/FAQpage.jsx";
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-      <Route path="/" element={
-          <>
-            <Registration />
-
-          </>
-        } />
-        <Route path="/Main" element={
-          <>
-            <Hero />
-            <ForWho />
-            <Competitions />
-            <Courses />
-            <News />
-            <Footer />
-          </>
-        } />
-        <Route path="/Competitions" element={
-          <>
-          <CompetitionHero />
-          <CompetitionTabs />
-          <СompetitionTabsNow/>
-          <CompetitionList/>
-          <Footer />
-          </>
-          } />
-          <Route path="/learning" element={
+        <Route
+          path="/"
+          element={
             <>
-            <LearningHero />
-            <BeginnerCourses/>
-            <BeginnerCoursesTabsNow/>
-            <BeginnerCourseslist/>
-            <Footer />
+              <Registration />
             </>
-            } />
-            <Route path="/forum" element={
+          }
+        />
+        <Route
+          path="/Main"
+          element={
             <>
-            <Footer />
+              <Hero />
+              <ForWho />
+              <Competitions />
+              <Courses />
+              <News />
+              <Footer />
             </>
-            } />
-            <Route path="/faq" element={
+          }
+        />
+        <Route
+          path="/Competitions"
+          element={
             <>
-            <Footer />
+              <CompetitionHero />
+              <CompetitionTabs />
+              <СompetitionTabsNow />
+              <CompetitionList />
+              <Footer />
             </>
-            } />
-            <Route path="/contact" element={
+          }
+        />
+        <Route
+          path="/learning"
+          element={
             <>
-            <Footer />
+              <LearningHero />
+              <BeginnerCourses />
+              <BeginnerCoursesTabsNow />
+              <BeginnerCourseslist />
+              <Footer />
             </>
-            
-            } />
-            <Route path="/Competition" element={
+          }
+        />
+        <Route
+          path="/forum"
+          element={
             <>
-            <ComHero/>
-            <TabMenu />
-            <Footer />
+              <Footer />
             </>
-            } />
-            <Route path="/Course" element={
+          }
+        />
+        <Route path="/faq" element={<FAQpage />} />
+        <Route
+          path="/contact"
+          element={
             <>
-            <Course/>
-            <TabMenuCourse />
-            <Footer />
+              <Footer />
             </>
-            } />
-            <Route path="/Registration" element={
+          }
+        />
+        <Route
+          path="/Competition"
+          element={
             <>
-            <Registration/>
+              <ComHero />
+              <TabMenu />
+              <Footer />
             </>
-            } />
-            <Route path="/Login" element={
+          }
+        />
+        <Route
+          path="/Course"
+          element={
             <>
-            <Login/>
+              <Course />
+              <TabMenuCourse />
+              <Footer />
             </>
-            } />
-
+          }
+        />
+        <Route
+          path="/Registration"
+          element={
+            <>
+              <Registration />
+            </>
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            <>
+              <Login />
+            </>
+          }
+        />
       </Routes>
-      
     </Router>
   );
 };
